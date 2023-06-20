c23868b08637c011b73d993f94b03ff4776c435d
GabrielPazz <gbrpaix@gmail.com>
2023-06-20 00:59:32 -0300

    Indented Classes

--- code/Base.java
+++ code/Base.java
@@ -1,4 +1,3 @@
-// Author: João dos Santos Cardoso de Jesus - 12522161564
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -7,74 +6,59 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 
-public abstract class Base 
-{
+public abstract class Base {
     protected Socket socket;
     protected BufferedReader bufferedReader;
     protected BufferedWriter bufferedWriter;
     protected String clientUsername;
     protected static int socketPort;
 
-    public Base(Socket socket) 
-    {
+    public Base(Socket socket) {
         super();
 
-        try 
-        {
-          this.socket = socket;
-          this.bufferedWriter = new BufferedWriter (new OutputStreamWriter(socket.getOutputStream()));
-          this.bufferedReader = new BufferedReader (new InputStreamReader(socket.getInputStream()));
-        }
-        catch (IOException e) 
-        {
+        try {
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException e) {
             LogError("Base", "Constructor", e);
             closeEverything(e);
         }
     }
 
-    public Base() 
-    {
+    public Base() {
         super();
     }
-    
-    protected void Print(String input)
-    {
+
+    protected void Print(String input) {
         System.out.println(input);
     }
 
-    protected void closeEverything(IOException e)
-    {
-        try 
-        {
-           closeEverything();
-        } 
-        catch (IOException errException) 
-        {
+    protected void closeEverything(IOException e) {
+        try {
+            closeEverything();
+        } catch (IOException errException) {
             LogError("Base", "closeEverything", e);
         }
     }
-    
-    protected void closeEverything() throws IOException
-    {
-        if (bufferedReader != null) 
-        {
+
+    protected void closeEverything() throws IOException {
+        if (bufferedReader != null) {
             bufferedReader.close();
         }
 
-        if (bufferedWriter != null) 
-        {
+        if (bufferedWriter != null) {
             bufferedWriter.close();
         }
 
-        if (socket != null) 
-        {
+        if (socket != null) {
             socket.close();
         }
     }
 
-    protected void LogError(String className, String methodName, Exception e)
-    {
-        String erroMessage = "Ocorreu um erro na classe " + className + ".Method:" + methodName + ".Details:" + e.getMessage() + " - " + e.toString();
+    protected void LogError(String className, String methodName, Exception e) {
+        String erroMessage = "Ocorreu um erro na classe " + className + ".Method:" + methodName + ".Details:"
+                + e.getMessage() + " - " + e.toString();
 
         Print(erroMessage);
 
--- code/Client.java
+++ code/Client.java
@@ -1,39 +1,32 @@
-// Author: João dos Santos Cardoso de Jesus - 12522161564
 
 import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Client extends Base implements Runnable
-{
-    public Client(Socket socket, String username)
-    {
+public class Client extends Base implements Runnable {
+    public Client(Socket socket, String username) {
         super(socket);
-        
-        this.clientUsername = username;       
+
+        this.clientUsername = username;
     }
 
-    public void sendMessage() 
-    {
-        try
-        {
+    public void sendMessage() {
+        try {
             bufferedWriter.write(clientUsername);
             bufferedWriter.newLine();
             bufferedWriter.flush();
 
             Scanner scanner = new Scanner(System.in);
 
-            while (socket.isConnected())
-            {
+            while (socket.isConnected()) {
                 String mensagem = scanner.nextLine();
-                
+
                 bufferedWriter.write(clientUsername + ": " + mensagem);
                 bufferedWriter.newLine();
                 bufferedWriter.flush();
 
-                if(mensagem.toLowerCase().contains("/sair"))
-                {
-                    System.out.print("\033[H\033[2J");  
+                if (mensagem.toLowerCase().contains("/sair")) {
+                    System.out.print("\033[H\033[2J");
                     System.out.flush();
 
                     bufferedWriter.write("/sair");
@@ -42,97 +35,77 @@ public class Client extends Base implements Runnable
 
                     Print("Você foi desconectado.");
                 }
-               
+
             }
 
             bufferedWriter.close();
             scanner.close();
 
-        } 
-        catch (IOException e) 
-        {
+        } catch (IOException e) {
             LogError("Client", "sendMessage.IOException", e);
             closeEverything(e);
         }
     }
 
     @Override
-    public void run() 
-    {
+    public void run() {
         String msgFromGroupChat;
 
-        while (socket.isConnected()) 
-        {
-            try 
-            {
+        while (socket.isConnected()) {
+            try {
                 msgFromGroupChat = bufferedReader.readLine();
 
-                if (msgFromGroupChat!=null && !msgFromGroupChat.isEmpty()) 
-                {
-                    if (msgFromGroupChat.toLowerCase().contains("/sair"))
-                    {
-                        closeEverything();                        
+                if (msgFromGroupChat != null && !msgFromGroupChat.isEmpty()) {
+                    if (msgFromGroupChat.toLowerCase().contains("/sair")) {
+                        closeEverything();
                         break;
-                    }
-                    else    
-                    {
+                    } else {
                         Print(msgFromGroupChat);
                     }
                 }
 
-            }
-            catch (IOException e) 
-            {
+            } catch (IOException e) {
                 LogError("Client", "Run.IOException", e);
                 closeEverything(e);
             }
 
         }
     }
-   
-    public static void main(String[] args) throws IOException 
-    {
+
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Servidor - Insira seu nome de usuario: ");   
+        System.out.print("Servidor - Insira seu nome de usuario: ");
 
         String username = scanner.nextLine();
 
-        if(username!=null && !username.isEmpty())
-        {          
-            while(username.toLowerCase().contains("sair"))
-            {
+        if (username != null && !username.isEmpty()) {
+            while (username.toLowerCase().contains("sair")) {
                 System.out.println("Por favor, informe um nome de usuario valido:");
                 username = scanner.nextLine();
             }
-            try 
-            {
-                Socket socket = new Socket("localhost", 1234);            
+            try {
+                Socket socket = new Socket("localhost", 1234);
 
                 Client client = new Client(socket, username);
 
                 Thread thread = new Thread(client);
                 thread.start();
 
-                System.out.print("\033[H\033[2J");  
+                System.out.print("\033[H\033[2J");
                 System.out.flush();
 
                 client.sendMessage();
 
                 scanner.close();
-            }
-            catch (Exception e)
-            {
-                if(e.getMessage().contains("Connection refused: connect"))
-                {
+            } catch (Exception e) {
+                if (e.getMessage().contains("Connection refused: connect")) {
                     System.out.println("Não foi possível realizar conexão ao localhost:1234");
-                }            
+                }
             }
-            
-        }
-        else   
-        {
+
+        } else {
             System.out.println("username null ou empty");
         }
-           
+
     }
 }
